10ebb5904c0773a81fcf30015822db455925934d
mc78 <mc78@outlook.de>
2015-06-24 10:33:20 +0200

    set color functions now check color value

--- VICColorfadeSetStartEnd.c
+++ VICColorfadeSetStartEnd.c
@@ -1,11 +1,11 @@
 #include "VICColorfade_internal.h"
 
 VICColorfadeErrorNo_t VICColorfadeSetLimits(VICColorfade_t *vcf, uint8_t startcolor, uint8_t endcolor){
-	if (startcolor<16) vcf->startcolor = startcolor;
-	else return VICCOLORFADEERR_CHARMODEANDCOLORGT7;
+	if (VICColorfadeCheckColorValue(startcolor)) vcf->startcolor = startcolor;
+	else return VICCOLORFADEERR_CVALUE;
 
-	if (endcolor<16) vcf->endcolor = endcolor;
-	else return VICCOLORFADEERR_CHARMODEANDCOLORGT7;
+	if (VICColorfadeCheckColorValue(endcolor)) vcf->endcolor = endcolor;
+	else return VICCOLORFADEERR_CVALUE;
 
 	VICColorfadeCalcReadPtr(vcf);
 	return VICCOLORFADEERR_OK;
